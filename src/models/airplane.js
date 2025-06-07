bd3526f6ba3b07a63335be6360289267d34dd0f7
Mihir D <mihirdongare1045@gmail.com>
2025-06-07 22:26:03 +0530

    UPDATE: EOD commit

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey:'airplaneId',
         onUpdate:'CASCADE',
         onDelete:'CASCADE'
-      }) 
+      })
     }
   }
   Airplane.init(
